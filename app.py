fd523e0b0211583eb0181cac1d6726a686a691cd
Ajitabh Tiwari <75995830+ajitabhtiwari@users.noreply.github.com>
2026-01-07 12:38:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-%%writefile app.py
+
 import streamlit as st
 import pandas as pd
 import numpy as np
-
+import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score
+from sklearn.ensemble import RandomForestClassifier
+from xgboost import XGBClassifier, XGBRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.ensemble import RandomForestClassifier
