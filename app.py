f9df47bf752ecfcdef68412ff56b0c2133fc4f4d
Ajitabh Tiwari <75995830+ajitabhtiwari@users.noreply.github.com>
2026-01-28 18:48:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -63,13 +63,13 @@ page = st.sidebar.radio(
 # ---------------------------------------------------
 # Sidebar – Incoming EDI Order
 # ---------------------------------------------------
-#st.sidebar.header("🧾 Incoming EDI Order")
+st.sidebar.header("🧾 Incoming EDI Order")
 
-#missing = st.sidebar.slider("Missing Mandatory Fields", 0, 3, 0)
-#invalid_ref = st.sidebar.slider("Invalid Reference Count", 0, 2, 0)
-#format_err = st.sidebar.slider("Format Error Count", 0, 2, 0)
-#partner_err = st.sidebar.slider("Partner Rule Violations", 0, 2, 0)
-#order_lines = st.sidebar.slider("Number of Order Lines", 1, 20, 5)
+missing = st.sidebar.slider("Missing Mandatory Fields", 0, 3, 0)
+invalid_ref = st.sidebar.slider("Invalid Reference Count", 0, 2, 0)
+format_err = st.sidebar.slider("Format Error Count", 0, 2, 0)
+partner_err = st.sidebar.slider("Partner Rule Violations", 0, 2, 0)
+order_lines = st.sidebar.slider("Number of Order Lines", 1, 20, 5)
 
 # ---------------------------------------------------
 # DQ Score
